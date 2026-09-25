2d3fcf9e9eece425fb2b6ea5f8156c712319492a
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-25 00:13:16 -0300

    feat: add ERP finance foundation

--- js/core/database/migrations/index.js
+++ js/core/database/migrations/index.js
@@ -1,6 +1,2 @@
 'use strict';
-const {runMigrations}=require('../migration-runner');
-const core=require('./001-core');
-const migrations=[core];
-function runErpMigrations(db,now){return runMigrations(db,migrations,now);}
-module.exports={migrations,runErpMigrations};
+const {runMigrations}=require('../migration-runner');const migrations=[require('./001-core'),require('./010-finance')];function runErpMigrations(db,now){return runMigrations(db,migrations,now);}module.exports={migrations,runErpMigrations};
