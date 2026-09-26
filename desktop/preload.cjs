8612d2ed639fd76f372536e1ae3b7c3713ddab6a
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-25 23:49:26 -0300

    feat: expose safe update IPC

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -2,6 +2,10 @@
 const {contextBridge,ipcRenderer}=require('electron');
 contextBridge.exposeInMainWorld('erpDesktop',Object.freeze({
   getBaseUrl:()=>ipcRenderer.invoke('erp:base-url'),
+  getUpdateStatus:()=>ipcRenderer.invoke('erp:update-status'),
+  checkForUpdates:()=>ipcRenderer.invoke('erp:update-check'),
+  downloadUpdate:()=>ipcRenderer.invoke('erp:update-download'),
+  installUpdate:()=>ipcRenderer.invoke('erp:update-install'),
   selectImportFile:()=>ipcRenderer.invoke('erp:select-import-file'),
   selectAttachment:()=>ipcRenderer.invoke('erp:select-attachment'),
   saveExportFile:input=>ipcRenderer.invoke('erp:save-export-file',input),
