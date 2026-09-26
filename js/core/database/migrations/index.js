1a8698cae704193a672c48d32b433ac96c39ad08
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-26 00:09:29 -0300

    chore: compatibilizar migrations de product readiness e almoxarifado

--- js/core/database/migrations/index.js
+++ js/core/database/migrations/index.js
@@ -1,5 +1,5 @@
 'use strict';
 const {runMigrations}=require('../migration-runner');
-const migrations=[require('./001-core'),require('./010-finance'),require('./020-master-data'),require('./021-inventory'),require('./030-procurement'),require('./040-sales-admin'),require('./050-finance-automation'),require('./060-eventbus'),require('./070-inventory-operations'),require('./075-auth-director'),require('./080-procurement-advanced'),require('./090-sales-admin-history'),require('./100-product-readiness')];
+const migrations=[require('./001-core'),require('./010-finance'),require('./020-master-data'),require('./021-inventory'),require('./030-procurement'),require('./040-sales-admin'),require('./050-finance-automation'),require('./060-eventbus'),require('./070-inventory-operations'),require('./075-auth-director'),require('./080-procurement-advanced'),require('./090-sales-admin-history'),require('./100-product-readiness'),require('./100-inventory-depth')];
 function runErpMigrations(db,now){return runMigrations(db,migrations,now);}
 module.exports={migrations,runErpMigrations};
